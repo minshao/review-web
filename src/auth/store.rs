737286f05710410bd876a9d8f7cd964c51305cc4
Min Shao <min.shao1988@gmail.com>
2024-02-12 12:56:22 -0800

    Update `access_token` interface

--- src/auth/store.rs
+++ src/auth/store.rs
@@ -1,8 +1,6 @@
 use super::AuthError;
 use crate::Store;
-use anyhow::{anyhow, bail, Result};
-use bincode::Options;
-use std::collections::HashSet;
+use anyhow::{anyhow, Result};
 
 /// Inserts a token into the store.
 ///
@@ -11,20 +9,7 @@ use std::collections::HashSet;
 /// Returns an error if the tokens in the store are invalid, if the token cannot be serialized, or
 /// if the store cannot be accessed.
 pub fn insert_token(store: &Store, token: &str, username: &str) -> Result<()> {
-    let map = store.access_token_map();
-    let tokens = map.get(username.as_bytes())?;
-    let value = if let Some(tokens) = tokens {
-        let mut tokens =
-            bincode::DefaultOptions::new().deserialize::<HashSet<String>>(tokens.as_ref())?;
-        tokens.insert(token.to_string());
-        tokens
-    } else {
-        HashSet::from([token.to_string()])
-    };
-    let value = bincode::DefaultOptions::new().serialize(&value)?;
-    map.put(username.as_bytes(), &value)?;
-
-    Ok(())
+    store.access_token_map().insert(username, token)
 }
 
 /// Revokes a token from the store.
@@ -36,20 +21,10 @@ pub fn insert_token(store: &Store, token: &str, username: &str) -> Result<()> {
 pub fn revoke_token(store: &Store, token: &str) -> Result<()> {
     let decoded_token = super::decode_token(token)?;
     let username = decoded_token.sub;
-    let map = store.access_token_map();
-    let value = map
-        .get(username.as_bytes())?
-        .ok_or_else(|| anyhow!("The given token does not exist"))?;
-    let mut tokens =
-        bincode::DefaultOptions::new().deserialize::<HashSet<String>>(value.as_ref())?;
-    if tokens.contains(token) {
-        tokens.remove(token);
-        let value = bincode::DefaultOptions::new().serialize(&tokens)?;
-        map.put(username.as_bytes(), &value)?;
-        Ok(())
-    } else {
-        bail!("The given token does not exist");
-    }
+    store
+        .access_token_map()
+        .revoke(&username, token)
+        .map_err(|_| anyhow!("The given token does not exist"))
 }
 
 pub(super) fn token_exists_in_store(
@@ -57,16 +32,8 @@ pub(super) fn token_exists_in_store(
     token: &str,
     username: &str,
 ) -> Result<bool, AuthError> {
-    let value = store
+    store
         .access_token_map()
-        .get(username.as_bytes())
-        .map_err(|_| AuthError::InvalidToken("Token not found in the database".into()))?;
-    if let Some(value) = value {
-        let tokens = bincode::DefaultOptions::new()
-            .deserialize::<HashSet<String>>(value.as_ref())
-            .map_err(|e| AuthError::Other(format!("An unexpected value in the database: {e}")))?;
-        Ok(tokens.contains(token))
-    } else {
-        Ok(false)
-    }
+        .contains(username, token)
+        .map_err(|_| AuthError::InvalidToken("Token not found in the database".into()))
 }
